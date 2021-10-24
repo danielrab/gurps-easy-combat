af85e16b9d1686748cd9d4a838a341de6f8d4df8
danielrab.work <danielrab.work@gmail.com>
2021-10-24 18:34:34 +0300

    add first setting

--- src/module/setup/settings.ts
+++ src/module/setup/settings.ts
@@ -1,3 +1,12 @@
+import { MODULE_NAME } from './constants.js';
+
 export function registerSettings(): void {
-  // Register any custom module settings here
+  game.settings.register(MODULE_NAME, 'maneuver-chooser-on-turn', {
+    scope: 'world',
+    type: Boolean,
+    default: true,
+    name: 'Open Maneuver Chooser when a new turn in combat is started',
+    hint: 'the Chooser will only be opened for players that own the token',
+    config: true,
+  });
 }
