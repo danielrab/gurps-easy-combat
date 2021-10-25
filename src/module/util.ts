c85b05635b5993d07df2303cf9bcc38170d8c03d
danielrab.work <danielrab.work@gmail.com>
2021-10-26 02:23:28 +0300

    add priority comparison

--- src/module/util.ts
+++ src/module/util.ts
@@ -41,3 +41,21 @@ export function isDefined<T>(value: T | undefined): value is T {
   if (value === undefined) return false;
   return true;
 }
+
+function getPriority(user: User, actor: Actor): number {
+  let priority = 0;
+  if (user.character === actor) priority += 100;
+  if (actor.testUserPermission(user, 'OWNER')) priority += 10;
+  if (user.isGM) priority -= 1;
+  if (!user.active) priority -= 1000;
+  return priority;
+}
+
+export function highestPriorityUsers(actor: Actor): User[] {
+  if (!game.users) {
+    throw new Error('game not initialized');
+  }
+  const priorities = new Map(game.users.map((user) => [user, getPriority(user, actor)]));
+  const maxPriority = Math.max(...priorities.values());
+  return game.users.filter((user) => priorities.get(user) === maxPriority);
+}
