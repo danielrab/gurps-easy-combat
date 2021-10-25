cd7992ea01794d53dbac9be34c280d382e543af1
danielrab.work <danielrab.work@gmail.com>
2021-10-26 01:54:03 +0300

    first full workflow prototype

--- src/module/applications/abstract/BaseActorController.ts
+++ src/module/applications/abstract/BaseActorController.ts
@@ -1,12 +1,41 @@
-export default abstract class BaseActorController extends Application {
+export default class BaseActorController extends Application {
   actor: Actor;
 
-  constructor(actor: Actor, options: { template: string; title: string } & Partial<Application.Options>) {
-    super(mergeObject(Application.defaultOptions, { resizable: true, ...options }));
+  static apps = new Map<string, BaseActorController>();
+
+  constructor(appName: string, actor: Actor, options: Partial<Application.Options>) {
+    const id = `${appName}-${actor.id}`;
+    super(mergeObject(Application.defaultOptions, { resizable: true, id, ...options }));
     this.actor = actor;
-    if (!this.actor) ui.notifications?.warn('You must have a character selected');
-    this.render(true);
+    if (!this.actor) {
+      throw new Error('no actor');
+    }
+    BaseActorController.apps.set(id, this);
   }
 
-  abstract getData(): Record<string, unknown> | Promise<Record<string, unknown>>;
+  async close(options?: Application.CloseOptions): Promise<void> {
+    await super.close(options);
+    BaseActorController.apps.delete(this.id);
+  }
+
+  static closeById(id: string): boolean {
+    const instance = BaseActorController.apps.get(id);
+    if (!instance) return false;
+    instance.close();
+    return true;
+  }
+
+  closeForEveryone(): void {
+    EasyCombat.socket.executeForEveryone('closeController', this.id);
+  }
+
+  static async closeAll(): Promise<void> {
+    await Promise.all(
+      [...this.apps.values()].map(async (app) => {
+        if (app instanceof this) {
+          await app.close();
+        }
+      }),
+    );
+  }
 }
---
+++ src/module/applications/maneuvers/Feint.ts
@@ -0,0 +1,3 @@
+import BaseActorController from '../abstract/BaseActorController';
+
+export default class AttackChooser extends BaseActorController {}
