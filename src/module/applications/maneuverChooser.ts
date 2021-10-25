339f73eb77780af23fa272f933f4dd8c84cef1e3
danielrab.work <danielrab.work@gmail.com>
2021-10-26 02:25:47 +0300

    use callbacks to call next application

--- src/module/applications/maneuverChooser.ts
+++ src/module/applications/maneuverChooser.ts
@@ -1,110 +1,133 @@
 import { MODULE_NAME } from '../setup/constants.js';
 import BaseActorController from './abstract/BaseActorController.js';
-import { AttackChooser } from './attackChooser.js';
+import AttackChooser from './attackChooser.js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Maneuvers from '/systems/gurps/module/actor/maneuver.js';
 
-type Maneuver = {
-  name: string;
+//#region types
+interface ManeuverInfo {
   tooltip: string;
   page: string;
+  callback?: (actor: Actor) => void;
+}
+interface Maneuver extends ManeuverInfo {
+  name: string;
   canAttack: boolean;
-};
+}
+interface GurpsManeuver {
+  changes: {
+    key: string;
+    mode: number;
+    priority: number;
+    value: string;
+  }[];
+  flags: {
+    gurps: {
+      alt: null;
+      defense: 'any' | 'none' | 'dodge-block';
+      fullturn: boolean;
+      icon: string;
+      move: 'step' | 'half' | 'full';
+      name: string;
+    };
+  };
+  icon: string;
+  id: 'maneuver';
+  label: string;
+}
+//#endregion
 
-const maneuvers: Record<string, Maneuver> = {
+const maneuversInfo: Record<string, ManeuverInfo> = {
   do_nothing: {
-    name: 'Do Nothing',
     tooltip: 'Take no action but recover from stun',
     page: 'B:364',
-    canAttack: false,
+    callback: () => game.combat?.nextTurn(),
   },
   move: {
-    name: 'Move',
     tooltip: 'Do nothing but move',
     page: 'B:364',
-    canAttack: false,
   },
   change_posture: {
-    name: 'Change Posture',
     tooltip: `Stand up, sit down, etc.`,
     page: 'B:364',
-    canAttack: false,
   },
   aim: {
-    name: 'Aim',
     tooltip: 'Aim a ranged weapon to get its Accuracy bonus',
     page: 'B:364',
-    canAttack: false,
   },
   evaluate: {
-    name: 'Evaluate',
     tooltip: 'Study a foe prior to a melee attack',
     page: 'B:364',
-    canAttack: false,
   },
   attack: {
-    name: 'Attack',
     tooltip: 'unarmed or with a weapon',
     page: 'B:365',
-    canAttack: true,
+    callback: (actor: Actor) => new AttackChooser(actor, { isMoving: false }, {}).render(true),
   },
   move_and_attack: {
-    name: 'Move and Attack',
     tooltip: 'Move and attack at a penalty',
     page: 'B:365',
-    canAttack: true,
+    callback: (actor: Actor) => new AttackChooser(actor, { isMoving: true }, {}).render(true),
   },
   feint: {
-    name: 'Feint',
     tooltip: 'Fake a melee attack',
     page: 'B:365',
-    canAttack: false,
   },
   allout_attack: {
-    name: 'All-out Attack',
     tooltip: 'Attack at a bonus or multiple times',
     page: 'B:365',
-    canAttack: true,
   },
   allout_defense: {
-    name: 'All-out Defense',
     tooltip: 'Increased or double defense',
     page: 'B:366',
-    canAttack: false,
   },
   ready: {
-    name: 'Ready',
     tooltip: 'Prepare a weapon or other item',
     page: 'B:366',
-    canAttack: false,
   },
   concentrate: {
-    name: 'Concentrate',
     tooltip: 'Focus on a mental task',
     page: 'B:366',
-    canAttack: false,
   },
   wait: {
-    name: 'Wait',
     tooltip: 'Hold yourself in readiness to act',
     page: 'B:366',
-    canAttack: false,
   },
 };
 
-export class ManeuverChooser extends BaseActorController {
+function getManeuversData(): Record<string, Maneuver> {
+  const gurpsManeuvers: Record<string, GurpsManeuver> = Maneuvers.getAllData();
+  return Object.fromEntries(
+    Object.entries(maneuversInfo).map(([key, maneuverInfo]: [string, ManeuverInfo]) => {
+      const maneuver = gurpsManeuvers[key];
+      const maneuverData: Maneuver = {
+        ...maneuverInfo,
+        name: game.i18n.localize(maneuver.label),
+        canAttack: key.includes('attack'),
+      };
+      return [key, maneuverData];
+    }),
+  );
+}
+
+export default class ManeuverChooser extends BaseActorController {
   static instance: ManeuverChooser;
 
-  constructor(actor: Actor) {
-    super(actor, {
-      title: 'Maneuvers Chooser',
+  constructor(actor: Actor, options?: Partial<Application.Options>) {
+    super('ManeuverChooser', actor, {
+      title: `Maneuver Chooser - ${actor.name}`,
       template: `modules/${MODULE_NAME}/templates/maneuverChooser.hbs`,
       width: 600,
-      resizable: true,
+      ...options,
     });
+    ManeuverChooser.instance = this;
   }
   getData(): { maneuvers: Record<string, Maneuver>; actor: Actor } {
-    return { maneuvers, actor: this.actor };
+    return { maneuvers: getManeuversData(), actor: this.actor };
   }
   activateListeners(html: JQuery): void {
+    super.activateListeners(html);
     html.find('.manuever_choice').on('click', (event) => {
       const element = $(event.currentTarget);
       const maneuverName = element.attr('value');
@@ -112,30 +135,13 @@ export class ManeuverChooser extends BaseActorController {
         ui.notifications?.error('no value on clicked element');
         return;
       }
-      const maneuver = maneuvers[maneuverName];
+      const maneuver = getManeuversData()[maneuverName];
       this.actor.replaceManeuver(maneuverName);
       ChatMessage.create({
         content: `${this.actor.name} uses the "${element.attr('name')}" maneuver [PDF:${element.attr('page')}]`,
       });
-      this.close();
-      EasyCombat.socket.executeForEveryone('closeManueverChooser', this.actor.id);
-      if (maneuver.canAttack) {
-        new EasyCombat.AttackChooser(this.actor).render(true);
-      }
+      this.closeForEveryone();
+      maneuver.callback?.(this.actor);
     });
-  }
-  static async closeIfActor(actorId: string | null): Promise<void> {
-    console.log(actorId);
-    if (this.instance.actor.id === actorId) {
-      this.instance.close();
-    }
-  }
-  static async show(actor: Actor): Promise<ManeuverChooser> {
-    if (!this.instance) {
-      this.instance = new this(actor);
-    }
-    this.instance.actor = actor;
-    this.instance.render(true);
-    return this.instance;
   }
 }
